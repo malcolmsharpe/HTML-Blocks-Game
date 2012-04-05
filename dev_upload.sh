a6a97152cda59290a9ba3027925198363f799380
Malcolm Sharpe <sharpe.malcolm@gmail.com>
2012-04-05 13:54:18 -0400

    Much smarter uploading.

--- dev_upload.sh
+++ dev_upload.sh
@@ -1,7 +1,6 @@
 PROJECT=blocks
+SRCS="$PROJECT.html $PROJECT.js $PROJECT.mf"
+DEST=masharpe@math.uwaterloo.ca:public_html/dev/
 
 python generate_manifest.py $PROJECT.mf
-scp $PROJECT.html masharpe@math.uwaterloo.ca:public_html/dev/$PROJECT.html
-scp $PROJECT.js masharpe@math.uwaterloo.ca:public_html/dev/$PROJECT.js
-scp $PROJECT.mf masharpe@math.uwaterloo.ca:public_html/dev/$PROJECT.mf
-ssh masharpe@math.uwaterloo.ca "cd public_html; cd dev; chmod a+rx $PROJECT.*"
+rsync --perms --progress $SRCS $DEST
--- release_upload.sh
+++ release_upload.sh
@@ -1,7 +1,6 @@
 PROJECT=blocks
+SRCS="$PROJECT.html $PROJECT.js $PROJECT.mf"
+DEST=masharpe@math.uwaterloo.ca:public_html/
 
 python generate_manifest.py $PROJECT.mf
-scp $PROJECT.html masharpe@math.uwaterloo.ca:public_html/$PROJECT.html
-scp $PROJECT.js masharpe@math.uwaterloo.ca:public_html/$PROJECT.js
-scp $PROJECT.mf masharpe@math.uwaterloo.ca:public_html/$PROJECT.mf
-ssh masharpe@math.uwaterloo.ca "cd public_html; chmod a+rx $PROJECT.*"
+rsync --perms --progress $SRCS $DEST
